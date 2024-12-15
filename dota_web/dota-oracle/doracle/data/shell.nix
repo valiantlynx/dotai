01907d1e429edb67911e17cfcf4440eda38c2184
valiantlynx <gormerykombo@gmail.com>
2024-12-15 14:42:02 +0100

    test reading the zip file

---
+++ dota_web/dota-oracle/doracle/data/shell.nix
@@ -0,0 +1,28 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  name = "python-environment";
+
+  buildInputs = [
+    pkgs.python3
+    pkgs.python3Packages.dash
+    pkgs.python3Packages.plotly
+    pkgs.python3Packages.pandas
+    pkgs.python3Packages.numpy
+    pkgs.python3Packages.matplotlib
+    pkgs.python3Packages.jupyter
+    pkgs.python3Packages.scikit-learn
+    pkgs.python3Packages.flask
+    pkgs.python3Packages.numba
+    pkgs.python3Packages.zipfile2
+    pkgs.python3Packages.tqdm
+    pkgs.uv
+  ];
+
+  shellHook = ''
+    echo "Welcome to your Python development environment!"
+    python --version
+  '';
+}
